3d93b84274cacdcbfe383ba561a95c7ea76d97c7
brchao <brchao@s.sfusd.edu>
2018-10-19 11:44:14 -0700

    OldMacdonald

--- Farm.java
+++ Farm.java
@@ -1,4 +1,17 @@
-class Farm 
-{     
-   //your code here
+class Farm  {     
+   private Animal[] aBunchOfAnimals;    
+   public Farm()     {       
+      aBunchOfAnimals = new Animal[3];
+      aBunchOfAnimals[0] = new Cow("cow","moo");
+      if(Math.random()<.5)           
+      aBunchOfAnimals[1] = new Chick("chick","cluck"); 
+      else
+      aBunchOfAnimals[1] = new Chick("chick","cheep");      
+      aBunchOfAnimals[2] = new Pig("pig","oink");    
+   }         
+   public void animalSounds()    {       
+     for (int nI=0; nI < aBunchOfAnimals.length; nI++)       {          
+       System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());       
+     }    
+   } 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,11 +1,7 @@
 
 public void setup()
 {
-	Cow c = new Cow("cow", "moo");   
-    System.out.println(c.getType() + " goes " + c.getSound());
-    Pig d = new Pig("pig", "oink");   
-    System.out.println(d.getType() + " goes " + d.getSound());   
-    Chick e = new Chick("chicken", "back home for thanksgiving");   
-    System.out.println(e.getType() + " goes " + e.getSound());       
+	Farm f = new Farm();
+	f.animalSounds();     
 }
 
